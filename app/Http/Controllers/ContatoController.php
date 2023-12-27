0dd453d69cea4f90484823c1ce59752324932b4b
pattiesanchez <pattuhwang42@gmail.com>
2023-12-27 17:56:04 -0300

    aula 129

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,36 +4,13 @@ namespace App\Http\Controllers;
 
 use App\SiteContato;
 use Illuminate\Http\Request;
+use App\MotivoContato;
 
 class ContatoController extends Controller 
 {
     public function contato(Request $request){
 
-        // echo '<pre>';
-        // print_r($request->all());
-        // echo '</pre>';
-        // echo $request->input('nome');
-        // echo '<br>';
-        // echo $request->input('email');
-
-        // $contato = new SiteContato();
-        // $contato->nome = $request->input('nome');
-        // $contato->telefone = $request->input('telefone');
-        // $contato->email = $request->input('email');
-        // $contato->motivo_contato = $request->input('motivo_contato');
-        // $contato->mensagem = $request->input('mensagem');
-        // #print_r($contato->getAttributes());
-        // $contato->save();
-
-        // $contato = new SiteContato();
-        // $contato->fill($request->all());
-        // $contato->save();
-
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação'
-        ];
+        $motivo_contatos = MotivoContato::all();
 
         return view('site.contato', ['titulo' => 'Contato (teste)', 'motivo_contatos' => $motivo_contatos] );
 
