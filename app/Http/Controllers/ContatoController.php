8a9fe6592de0238bc3b3dcee3d86598fef5eedb8
pattiesanchez <pattuhwang42@gmail.com>
2023-12-28 18:14:52 -0300

    secao concluida

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -17,15 +17,29 @@ class ContatoController extends Controller
     }
 
     public function salvar(Request $request){
-
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000',
-        ]);
+        ];
 
+        $feedback = [
+            'nome.required' => 'O campo nome deverá ser preenchido',
+            'nome.min' => 'O nome deverá conter no mínimo 3 caracteres',
+            'nome.max' => 'O nome excedeu o número máximo de caracteres',
+            'nome.unique' => 'Este nome já existe na base de dados',
+            'telefone.required' => 'O campo telefone deverá ser preenchido',
+            'email.email' => 'O campo deverá ser um e-mail válido',
+            'motivo_contatos_id.required' => 'O campo motivo do contato deverá ser preenchido',
+            'mensagem.required' => 'O campo mensagem deverá ser preenchido',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+        ];
+    
+        $request->validate($regras, $feedback);
+    
         SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
