46517fc577309ba47717daf0bb743ccf7063457d
pattiesanchez <pattuhwang42@gmail.com>
2024-01-02 20:51:42 -0300

    atualizando com aula 149 e 150

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,14 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
-    public function index() {
-        return view('site.login', ['titulo' => 'login']);
+    public function index(Request $request) {
+        $erro = '';
+        if($request->get('erro') == 1){ 
+            $erro = 'Usuário ou senha não existem';
+        }
+        return view('site.login', ['titulo' => 'login', 'erro' =>  $erro]);
     }
 
-    public function autenticar(){
-        return 'chegamos ate aqui';
+    public function autenticar(Request $request){
+
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        $feedback = [
+            'usuario.email' => 'O campo de email é obrigatório!',
+            'senha.required' => 'O campo senha é obrigatório!'
+        ];
+
+        $request->validate($regras, $feedback);
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuario: $email, Senha: $password";
+
+        $user = new User();
+
+        $usuario = $user->where("email", $email)->where("password", $password)->get()->first();
+        if(isset($usuario->name)){
+            echo 'Usuário existe na base de dados';
+        } else {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
     }
 }
