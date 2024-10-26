cecf67cdbd3057deb9f0d42ee1d18f5790f3d99c
pattiesanchez <pattuhwang42@gmail.com>
2024-10-25 22:17:36 -0300

    aula 163 e 164

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,7 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
     Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
     Route::get('/fornecedor/editar/{id}/{msg?}', 'FornecedorController@editar')->name('app.fornecedor.editar');
     Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name('app.fornecedor.excluir');
-
-    Route::resource('produto', 'ProdutoController');
+    Route::resource('produto', 'ProdutoController'); 
    
 });
 
