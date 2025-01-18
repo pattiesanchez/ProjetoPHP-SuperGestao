0899f5ed1f5b9a4a94328dea54655eaf22255897
pattiesanchez <pattuhwang42@gmail.com>
2025-01-17 21:55:06 -0300

    aula 166

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -40,7 +40,8 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
